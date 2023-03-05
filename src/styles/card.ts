6b94f405e541c4fb33429d7708ff825e85e95027
seyoungjoy <diddpwl80@gmail.com>
2023-03-05 11:42:03 +0900

    fix : 오타 삭제

--- src/styles/card.ts
+++ src/styles/card.ts
@@ -55,7 +55,7 @@ export const S = {
   `,
   CardBottom: styled.div`
     width: 100%;
-    height: 100%;=
+    height: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
